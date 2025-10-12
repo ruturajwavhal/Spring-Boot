90509bf2b09df365159f75d2193f1d1a16b23bd3
ruturajwavhal <ruturajwavhal10@gmail.com>
2025-10-12 16:50:32 +0530

    project created and git updated

---
+++ rest-api-demo-1/src/main/java/com/vednexgen/rest_api_demo_1/RestApiDemoApplication.java
@@ -0,0 +1,14 @@
+package com.vednexgen.rest_api_demo_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiDemoApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(RestApiDemoApplication.class, args);
+	}
+
+}
