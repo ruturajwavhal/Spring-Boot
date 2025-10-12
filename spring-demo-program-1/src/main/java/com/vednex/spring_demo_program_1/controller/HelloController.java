8ee5f7428b48f3ee63a000929444620d0a446c5a
ruturajwavhal <ruturajwavhal10@gmail.com>
2025-10-12 15:16:46 +0530

    initial commit

--- spring-demo-program-1/src/main/java/com/vednex/spring_demo_program_1/controller/HelloController.java
+++ spring-demo-program-1/src/main/java/com/vednex/spring_demo_program_1/controller/HelloController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
         @GetMapping("/hello/{name}")
         public String sayHelloToName(@PathVariable String name) {
-            return "Hello, " + name + " ! ";
+            return "Hello, " + name + " !";
         }
 
     }
